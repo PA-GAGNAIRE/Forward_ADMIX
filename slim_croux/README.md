0007c5d7ff6dce8d56a04f8a888b7373a6e16fc4
camille roux <camille.roux.1983@gmail.com>
2018-10-05 03:06:27 +0200

    Add files via upload

--- slim_croux/README.md
+++ slim_croux/README.md
@@ -1,1 +1,11 @@
+# Launch the slim script 'test_v1.slim', compute the ancestry along a chromosome and plot it  
+./launch_sim.py nPop1=10000 nPop2=10000 nPop3=20000 Lchro=100000000 outputFile=TEST3.tree slimScript=test_v1.slim nSampleAncestry=1000  
+*nPop1* = number of individuals in pop1  
+*nPop2* = number of individuals in pop2 
+*nPop3* = number of individuals in pop3  
+*Lchro* = length of the simulated chrosomose  
+*outputFile* = name of the slim's output containing the tree  
+*slimScript* = name of the slim's script containing the modele to simulate  
+*nSampleAncestry* = number of individuals that ancestry_proportions_v2.py will sample  
+  
 
---
+++ slim_croux/launch_sim.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import os
+import sys
+
+for i in sys.argv:
+	i = i.strip().split('=')
+	if i[0] == 'nPop1':
+		nPop1 = int(i[1])
+	if i[0] == 'nPop2':
+		nPop2 = int(i[1])
+	if i[0] == 'nPop3':
+		nPop3 = int(i[1])
+	if i[0] == 'Lchro':
+		Lchro = int(i[1])
+	if i[0] == 'outputFile':
+		outputFile = "'" + i[1] + "'"
+	if i[0] == 'slimScript':
+		slimScript = i[1]
+	if i[0] == 'nSampleAncestry':
+		nSampleAncestry = int(i[1])
+
+# Run slim
+print('Run slim')
+cmd = 'slim -d nPop1={0} -d nPop2={1} -d nPop3={2} -d Lchro={3} -d "outputFile={4}" {5}'.format(nPop1, nPop2, nPop3, Lchro, outputFile, slimScript)
+os.system(cmd)
+
+# Run "ancestry_proportions.py"
+print('\nRun ancestry_proportions_v3.py')
+cmd = './ancestry_proportions_v3.py {0} {1}'.format(outputFile, nSampleAncestry)
+os.system(cmd)
+
+# Plot statistics
+print('\nplot_ancestry.R')
+cmd = './plot_ancestry.R file={0}'.format(outputFile.split('.')[0].replace("'",""))
+os.system(cmd)
+
